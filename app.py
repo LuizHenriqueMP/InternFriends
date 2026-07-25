5d0744ea20960130152ee33faa3404c9f9301682
LuizHenriqueMP <98365070+LuizHenriqueMP@users.noreply.github.com>
2026-07-25 14:36:35 -0300

    Criação de usuario e conexão com o banco

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from models.Usuario import Usuario
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/criarUsuario')
+def criarUsuario():
+    usuario = Usuario('milena.jung@aluno.feliz.ifrs.edu.br', '26180915')
+    return usuario.criarUsuario()
